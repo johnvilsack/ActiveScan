6acce68a191895b6865d58aaadee8b78be990805
johnvilsack <vilsack@gmail.com>
2015-08-06 12:24:12 -0500

    Fixing create script

---
+++ create.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+CORDOVA_NAME="ActiveScan";
+CORDOVA_ID="com.activesportsinc.activescan";
+AUTHOR_NAME="John Vilsack";
+AUTHOR_EMAIL="vilsack@gmail.com";
+AUTHOR_WEBSITE="www.johnvilsack.com";
+
+# Create Project 
+cordova create $CORDOVA_NAME $CORDOVA_ID;
+
+# Change name to src
+mv $CORDOVA_NAME src;
+
+# Move customizable files
+yes | cp -f config.xml src/
+yes | cp -f splash.png src/
+yes | cp -f icon.png src/
+
+cd src;
+
+# Alter config.xml settings
+sed -i.sed "s/CORDOVA_NAME/$CORDOVA_NAME/g" config.xml
+sed -i.sed "s/CORDOVA_ID/$CORDOVA_ID/g" config.xml
+sed -i.sed "s/AUTHOR_NAME/$AUTHOR_NAME/g" config.xml
+sed -i.sed "s/AUTHOR_EMAIL/$AUTHOR_EMAIL/g" config.xml
+sed -i.sed "s/AUTHOR_WEBSITE/$AUTHOR_WEBSITE/g" config.xml
+rm config.xml.sed
+
+# Get the Linea Plugin
+cordova plugins add https://github.com/johnvilsack/ActiveScan-Cordova-Plugin-Linea-Pro.git;
+
+# Get the proper index.js to activate it
+wget -r -O src/www/js/index.js https://gist.githubusercontent.com/johnvilsack/44cb8f6fd9328e327ff9/raw/9244ed88161bc6f01877891028ce702ed1654e6f/index.js;
+
+# Build for IOS
+cordova platforms add ios;
+cordova build ios;
+cd ../;
+
+# Create shortcut to XCode
+ln -s src/platforms/ios/$CORDOVA_NAME.xcodeproj;
--- initActiveScan.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-cd ~/Desktop;
-cordova create ActiveScan com.activesportsinc.activescan;
-cd ActiveScan;
-cordova plugins add https://github.com/aaronthorp/lineapro-phonegap-plugin.git;
-wget -r -O ~/Desktop/ActiveScan/www/js/index.js https://gist.githubusercontent.com/johnvilsack/44cb8f6fd9328e327ff9/raw/9244ed88161bc6f01877891028ce702ed1654e6f/index.js;
-cordova platforms add ios;
-cordova build ios;
