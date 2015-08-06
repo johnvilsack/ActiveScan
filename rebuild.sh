754073ddf28875f0befa0fa8b6ff711994902289
johnvilsack <vilsack@gmail.com>
2015-08-06 15:46:16 -0500

    Minor

--- rebuild.sh
+++ rebuild.sh
@@ -4,5 +4,7 @@ source includes.sh
 cd src;
 cordova platforms remove ios;
 cordova platforms add ios;
+cd ..;
 cp default.plist src/platforms/ios/$CORDOVA_NAME/$CORDOVA_NAME-Info.plist;
+cd src;
 cordova build ios;
